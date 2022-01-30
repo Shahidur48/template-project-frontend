fe6e03d96445faea7d7eb04f1d9f13a65c40c7cc
Rahaman <mdshahidur_rahaman1@baylor.edu>
2022-01-30 00:05:02 -0600

    fix: add backend link for the axios call

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const USERS_REST_API_URL = 'http://localhost:8080/user/users';
+const USERS_REST_API_URL = 'http://localhost:80/user/users';
 
 class UserService {
     getUsers(){
